1d401a857df3659881ba087ef04a96a45dbc992b
talles <talles17.a@hotmail.com>
2021-12-18 13:17:46 -0300

    fix: fix header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react';
+import Image from 'next/image'
 
 export function Header() {
 
   return (
     <Flex h={[50, 81]} justifyContent="center">
-      <Image src="logo.png" alt="logo" />
+      <Image src="/logo.png" alt="logo" height={150} width={150} />
     </Flex>
   )
 }
