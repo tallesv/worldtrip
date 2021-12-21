57f172d238357fbdf23f2633a65e671b5ea9933f
talles <talles17.a@hotmail.com>
2021-12-20 21:42:33 -0300

    feat: add redirect to continent page

--- src/components/ContinentsCarousel/index.tsx
+++ src/components/ContinentsCarousel/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Text, Flex } from '@chakra-ui/react';
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useRouter } from 'next/router'
 
 // Import Swiper styles
 import "swiper/css";
@@ -19,31 +20,41 @@ const continents = [
   {
     title: 'Europa',
     subTitle: 'O continente mais antigo',
-    image: 'europe-carousel.png'
+    image: 'europe-carousel.png',
+    url: 'europe'
   },
   {
     title: 'América',
     subTitle: 'Tudo no seu alcance',
-    image: 'america-carousel.jpg'
+    image: 'america-carousel.jpg',
+    url: 'america'
   },
   {
     title: 'Ásia',
     subTitle: 'Descobertas infinitas',
-    image: 'asia-carousel.jpg'
+    image: 'asia-carousel.jpg',
+    url: 'asia'
   },
   {
     title: 'África',
     subTitle: 'Inspirando novas maneiras',
-    image: 'africa-carousel.jpg'
+    image: 'africa-carousel.jpg',
+    url: 'africa'
   },
   {
     title: 'Oceania',
     subTitle: 'Onde a felicidade encontra você',
-    image: 'oceania-carousel.jpg'
+    image: 'oceania-carousel.jpg',
+    url: 'oceania'
   },
 ]
 
 export function ContinentsCarousel() {
+  const { push } = useRouter();
+
+  function handleNavigateToContinetPage(url: string) {
+    push(`/continent/${url}`)
+  }
 
   return (
     <Box
@@ -59,7 +70,7 @@ export function ContinentsCarousel() {
         className="mySwiper"
       >
         {continents.map(continent => 
-          <SwiperSlide key={continent.title} data-hash={continent.title}>
+          <SwiperSlide key={continent.title} data-hash={continent.title} onClick={() => handleNavigateToContinetPage(continent.url)}>
             <Flex
               bgImage={`url(/${continent.image})`}
               bgSize="cover"
