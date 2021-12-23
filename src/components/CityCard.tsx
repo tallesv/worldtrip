dbe74795044b60ac3d1b898df13efdbfdbd10780
talles <talles17.a@hotmail.com>
2021-12-23 01:04:37 -0300

    feat: add continets data to pages

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -2,21 +2,26 @@ import { Box, BoxProps, Flex, Text } from '@chakra-ui/react';
 import Image from 'next/image'
 
 interface CityCardProps extends BoxProps {
-
+  city: {
+    name: string,
+    country: string,
+    cityImage: string,
+    countryImage: string,
+  }
 }
 
-export default function CityCard({ ...rest }: CityCardProps) {
+export default function CityCard({ city, ...rest }: CityCardProps) {
 
   return (
     <Box {...rest}>
-      <Image src="/london.png" alt="london" width={256} height={173} />
+      <Image src={city.cityImage} alt={city.name} width={256} height={173} />
 
       <Flex p={4} justifyContent="space-between" alignItems="center">
         <Flex direction="column">
-          <Text fontSize={20} fontWeight="600">Londres</Text>
-          <Text mt={2} fontSize={16} fontWeight="500" color="gray.450">Reino Unido</Text>
+          <Text fontSize={20} fontWeight="600">{city.name}</Text>
+          <Text mt={2} fontSize={16} fontWeight="500" color="gray.450">{city.country}</Text>
         </Flex>
-        <Image src="/uk.png" alt="uk" width={30} height={30} />
+        <Image src={city.countryImage} alt={city.country} width={30} height={30} />
       </Flex>
     </Box>
   );
--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -3,41 +3,57 @@ import { InfoOutlineIcon } from '@chakra-ui/icons';
 import { Header } from "../../components/Header";
 import CityCard from "../../components/CityCard";
 
+import continents from "../../utils/continent";
+import { GetStaticPaths } from "next";
+
+interface City {
+  name: string,
+  country: string,
+  cityImage: string,
+  countryImage: string,
+}
+interface Continent {
+  continent: string,
+  about: string,
+  banner: string,
+  contriesAmount: number,
+  languagesAmount: number,
+  citiesAmount: number,
+  cities: City[],
+}
 
 
-export default function Continent () {
+export default function Continent ({ continent, about, banner, contriesAmount, languagesAmount, citiesAmount, cities }: Continent) {
   return (
     <Box>
       <Header />
 
       <Flex
-        bgImage="url(/europe-banner.png)"
+        bgImage={`url(${banner})`}
         bgSize="cover"
         h={[150, 450]}
         p={20}
         justifyContent={["center", "flex-start"]}
         alignItems={["center", "flex-end"]}
       >
-        <Text fontSize={32} color="gray.50" fontWeight={600}>Europa</Text>
+        <Text fontSize={32} color="gray.50" fontWeight={600}>{continent}</Text>
       </Flex>
 
       <Flex mx={10} direction="column" alignItems="flex-start">
         <Flex my={10} w="100%" direction={{ base: 'column', lg: 'row' }} justifyContent="space-around" alignItems="center">
-          <Text maxW="600" fontSize={22} lineHeight="36px">
-            A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
-          </Text>
+          <Text maxW="600" fontSize={22} lineHeight="36px">{about}</Text>
 
           <Flex my={{ base: 10, lg: 0 }} justifyContent="space-between">
             <Flex mx={[4, 10]} direction="column">
-              <Text fontSize={[36, 46]} fontWeight="bold" color="yellow.300">50</Text>
+              <Text fontSize={[36, 46]} fontWeight="bold" color="yellow.300" alignSelf="center">{contriesAmount}</Text>
               <Text fontSize={[18, 20]} fontWeight="bold"> países</Text>
             </Flex>
             <Flex mx={[4, 10]} direction="column">
-              <Text fontSize={[36, 46]} fontWeight="bold" color="yellow.300">60</Text>
+              <Text fontSize={[36, 46]} fontWeight="bold" color="yellow.300" alignSelf="center">{languagesAmount}</Text>
               <Text fontSize={[18, 20]} fontWeight="bold">línguas</Text>
             </Flex>
             <Flex mx={[4, 10]} direction="column">
-              <Text fontSize={[36, 46]} fontWeight="bold" color="yellow.300">27</Text>
+              <Text fontSize={[36, 46]} fontWeight="bold" color="yellow.300" alignSelf="center">{citiesAmount}</Text>
               <Flex alignItems="center">
                 <Text mr={2} fontSize={[18, 20]} fontWeight="bold">cidades +100</Text>
                 <Tooltip label='154 cidades' fontSize="md">
@@ -52,10 +68,9 @@ export default function Continent () {
           <Text my={[10]} fontSize={36} fontWeight="500">Cidades +100</Text>
 
           <Flex direction={['column', 'row']} justifyContent={['center', 'flex-start']} flexWrap="wrap">
-            <CityCard w={256} h={279} mr={45} mb={45} bgColor="white" border="1px" borderRadius={6} borderColor="yellow.150"/>
-            <CityCard w={256} h={279} mr={45} mb={45} bgColor="white" border="1px" borderRadius={6} borderColor="yellow.150"/>
-            <CityCard w={256} h={279} mr={45} mb={45} bgColor="white" border="1px" borderRadius={6} borderColor="yellow.150"/>
-            <CityCard w={256} h={279} mr={45} mb={45} bgColor="white" border="1px" borderRadius={6} borderColor="yellow.150"/>
+            {cities?.map(city => (
+              <CityCard key={city.name} city={city} w={256} h={279} mr={[45]} mb={45} bgColor="white" border="1px" borderRadius={6} borderColor="yellow.150"/>
+            ))}
           </Flex>
         </Flex>
       </Flex>
@@ -63,3 +78,21 @@ export default function Continent () {
   );
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = continents.map(continent => ({ params: { continent: continent.url } }));
+
+  return {
+      paths, //indicates that no page needs be created at build time
+      fallback: 'blocking' //indicates the type of fallback
+  }
+}
+
+export async function getStaticProps({ params } : any) {
+  const { continent: url } = params;
+  const findContinent = continents.find(continent => continent.url === url);
+  return { 
+    props: {
+      ...findContinent
+    } 
+  }
+}
